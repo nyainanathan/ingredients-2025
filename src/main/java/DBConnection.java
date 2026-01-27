1006db788224427fc12d249c5b985576caabc524
A. Ny Aina Nathan <nyaina.nathan.1@gmail.com>
2026-01-27 14:54:42 +0300

    chore: new_schema sql file for db tables updates

--- src/main/java/DBConnection.java
+++ src/main/java/DBConnection.java
@@ -6,10 +6,10 @@ public class DBConnection {
 
     public Connection getConnection() {
         try {
-            String jdbcURl = System.getenv("JDBC_URl"); //
-            String user = System.getenv("USER"); //mini_dish_db_manager
-            String password = System.getenv("PASSWORD"); //123456
-            return DriverManager.getConnection("jdbc:postgresql://localhost:5432/mini_dish_db", "postgres", "postgres");
+            String jdbcURl = System.getenv("JDBC_URL");
+            String user = System.getenv("USER");
+            String password = System.getenv("PASSWORD");
+            return DriverManager.getConnection(jdbcURl, user, password);
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
---
+++ src/main/resources/sql/new_schema.sql
@@ -0,0 +1,13 @@
+create type unit_type as enum('PCS', 'KD', 'L');
+
+create table DishIngredients(
+    id serial primary key ,
+    id_dish int references dish(id),
+    id_ingredient int references ingredient(id),
+    quantity_required numeric(10,2 ),
+    unit unit_type
+);
+
+alter table ingredient drop column  id_dish;
+
+alter table ingredient drop column  required_quantity;
